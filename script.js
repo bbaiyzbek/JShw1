d37616193b4607c1bb0d62cf46c32da54c80cfd8
BaQuna <imbaquna@gmail.com>
2023-07-10 15:46:35 +0600

    homework js1

---
+++ script.js
@@ -0,0 +1,49 @@
+
+// let a=4
+// let b =55
+//
+// console.log(a+b)
+
+// let a=4
+// let b =55
+//
+// console.log(b-a)
+
+// let a=4
+// let b =55
+//
+// console.log(a*b)
+
+// let a=4
+// let b =55
+//
+// console.log(b/a)
+
+// let a=4
+// let b =55
+//
+// console.log(b%a)
+
+// let a="Hello"
+// let b ="Bakulya"
+//
+// console.log(a+" "+b)
+
+// let a=4
+// let b ="hello"
+//
+// console.log(a/b)
+
+// let a=5
+// let b ="5"
+//
+// console.log(a+ +b)
+
+// let a=5
+// let b ="5"
+//
+// console.log(Number(b)+a)
+
+let c=2
+let b
+console.log(b)
